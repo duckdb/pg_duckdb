c179399b4faec4458f079ad39be0b3a1a1da1540
Yves <yves@motherduck.com>
2024-08-06 18:02:06 -0400

    wip - handle multiple connection

--- src/pgduckdb_bgw.cpp
+++ src/pgduckdb_bgw.cpp
@@ -3,6 +3,7 @@ extern "C" {
 #include "fmgr.h"
 #include "postmaster/bgworker.h"
 #include "postmaster/interrupt.h"
+#include "postmaster/postmaster.h"
 #include "storage/latch.h"
 #include "storage/proc.h"
 #include "storage/procarray.h"
@@ -18,16 +19,6 @@ extern "C" {
 #include "libpq/pqmq.h"
 }
 
-/*
-  TODOS:
-  - create an array of queues per connection
-    -> how to identify a connection?
-    -> can we have a hook on new connection and only create queue then?
-    -> close / clean up queues when connection is closed
-    -> recycle/reuse queues?
-
-*/
-
 #include "pgduckdb/pgduckdb.h"
 
 #define WAIT_EVENT_PGDUCKDB_BGW_MAIN 1984
@@ -39,14 +30,22 @@ typedef struct
     pid_t worker_pid;
     LWLock lock;
     Latch *worker_latch;
-    dsm_handle seg;
+    dsm_handle connection_seg[MAX_BACKENDS];
+    LWLock	 state_version_lock;
+    uint32_t state_version;
 } PGDuckDBSharedState;
 
 typedef struct
 {
-    shm_mq_handle *input_queue;
-    shm_mq_handle *output_queue;
+    shm_mq_handle *command_queue;
+    shm_mq_handle *results_queue;
 } PGDuckDBInternalState;
+
+typedef struct
+{
+    BackendId backend_id;
+    Latch *latch;
+} PGDuckDBConnectionState;
 
 static PGDuckDBSharedState *pgduckdb_state = NULL;
 static PGDuckDBInternalState *pgduckdb_internal_state = NULL;
@@ -64,46 +63,60 @@ pgduckdb_shmem_request(void)
     elog(INFO, "pgduckdb_shmem_request -> done");
 }
 
-static void pgduckdb_create_shm_queue() {
+static dsm_handle pgduckdb_create_connection_shm_segment() {
     LWLockAcquire(&pgduckdb_state->lock, LW_EXCLUSIVE);
-    elog(INFO, "[bgw] pgduckdb_init_shmem -> setting worker_pid=%d", MyProcPid);
-    pgduckdb_state->worker_pid = MyProcPid;
-    pgduckdb_state->worker_latch = MyLatch;
+    elog(INFO, "[con] pgduckdb_create_connection_shm_segment -> starting");
 
     // Compute queue size
     shm_toc_estimator estimator;
-    shm_toc_estimate_chunk(&estimator, SHM_QUEUE_SIZE);
-    shm_toc_estimate_chunk(&estimator, SHM_QUEUE_SIZE);
+    shm_toc_estimate_chunk(&estimator, SHM_QUEUE_SIZE); // command queue
+    shm_toc_estimate_chunk(&estimator, SHM_QUEUE_SIZE); // results queue
+    shm_toc_estimate_chunk(&estimator, sizeof(PGDuckDBInternalState)); // internal state
+
     Size segsize = shm_toc_estimate(&estimator);
+    elog(INFO, "[con] pgduckdb_create_connection_shm_segment -> will allocate %zu", segsize);
 
     // Allocate shared memory
     dsm_segment *seg = dsm_create(segsize, 0);
     shm_toc *toc = shm_toc_create(PG_DUCKDB_MAGIC, dsm_segment_address(seg), segsize);
 
-    // Create queue to receive messages
+    pgduckdb_internal_state = (PGDuckDBInternalState *) palloc0(sizeof(PGDuckDBInternalState));
+
+    // Create queue to send messages
     {
         shm_mq *mq = shm_mq_create(shm_toc_allocate(toc, SHM_QUEUE_SIZE), SHM_QUEUE_SIZE);
         shm_toc_insert(toc, 0, mq);
-        shm_mq_set_receiver(mq, MyProc);
-        pgduckdb_internal_state->input_queue = shm_mq_attach(mq, seg, NULL);
-        elog(INFO, "[bgw] pgduckdb_init_shmem -> created queue=%p, seg=%u", pgduckdb_internal_state->input_queue, pgduckdb_state->seg);
+        shm_mq_set_sender(mq, MyProc);
+        pgduckdb_internal_state->command_queue = shm_mq_attach(mq, seg, NULL);
+        elog(INFO, "[con] pgduckdb_create_connection_shm_segment -> created queue=%p, seg=%p", pgduckdb_internal_state->command_queue, seg);
     }
 
-    // Create queue to send messages back
+    // Create queue to receive messages back
     {
         shm_mq *mq = shm_mq_create(shm_toc_allocate(toc, SHM_QUEUE_SIZE), SHM_QUEUE_SIZE);
         shm_toc_insert(toc, 1, mq);
-        shm_mq_set_sender(mq, MyProc);
-        pgduckdb_internal_state->output_queue = shm_mq_attach(mq, seg, NULL);
-        elog(INFO, "[bgw] pgduckdb_init_shmem -> created queue=%p, seg=%u", pgduckdb_internal_state->output_queue, pgduckdb_state->seg);
+        shm_mq_set_receiver(mq, MyProc);
+        pgduckdb_internal_state->results_queue = shm_mq_attach(mq, seg, NULL);
+        elog(INFO, "[con] pgduckdb_create_connection_shm_segment -> created queue=%p, seg=%p", pgduckdb_internal_state->results_queue, seg);
     }
 
-    pgduckdb_state->seg = dsm_segment_handle(seg);
+    PGDuckDBConnectionState* s = (PGDuckDBConnectionState*)shm_toc_allocate(toc, sizeof(PGDuckDBConnectionState));
+    s->backend_id = MyBackendId;
+    s->latch = MyLatch;
+    shm_toc_insert(toc, 2, s);
+
+    dsm_pin_mapping(seg);
+
+    elog(INFO, "[con] pgduckdb_create_connection_shm_segment -> inserted PGDuckDBConnectionState");
+
+    dsm_handle hdl = dsm_segment_handle(seg);
 
     LWLockRelease(&pgduckdb_state->lock);
+
+    return hdl;
 }
 
-static bool pgduckdb_init_shmem(bool called_from_bgw = false)
+static bool pgduckdb_init_shmem(void)
 {
     bool found;
 
@@ -115,13 +128,14 @@ static bool pgduckdb_init_shmem(bool called_from_bgw = false)
     {
         /* First time: initializing empty */
         LWLockInitialize(&pgduckdb_state->lock, LWLockNewTrancheId());
+        LWLockInitialize(&pgduckdb_state->state_version_lock, LWLockNewTrancheId());
         pgduckdb_state->worker_pid = InvalidPid;
         pgduckdb_state->worker_latch = NULL;
-        pgduckdb_state->seg = DSM_HANDLE_INVALID;
-    }
+        for (uint32_t i = 0; i < MAX_BACKENDS; ++i) {
+            pgduckdb_state->connection_seg[i] = DSM_HANDLE_INVALID;
+        }
 
-    if (called_from_bgw) {
-        pgduckdb_create_shm_queue();
+        pgduckdb_state->state_version = 0;
     }
 
     LWLockRelease(AddinShmemInitLock);
@@ -134,7 +148,7 @@ static bool pgduckdb_init_shmem(bool called_from_bgw = false)
 void
 duckdb_init_bgw(void)
 {
-    elog(INFO, "duckdb_init_bgw -> starting");
+    elog(INFO, "duckdb_init_bgw -> starting MyBackendId=%d", MyBackendId);
 
     prev_shmem_request_hook = shmem_request_hook;
     shmem_request_hook = pgduckdb_shmem_request;
@@ -158,9 +172,6 @@ extern "C" {
 
 PG_FUNCTION_INFO_V1(send_message);
 
-static shm_mq_handle *commands_queue = NULL;
-static shm_mq_handle *results_queue = NULL;
-
 Datum
 send_message(PG_FUNCTION_ARGS)
 {
@@ -169,58 +180,52 @@ send_message(PG_FUNCTION_ARGS)
     if (strlen(message) >= 2048)
         ereport(ERROR, (errmsg("Message too long")));
 
-    elog(INFO, "[cli] send_message -> received '%s' | shstate=%p | MyProcPid=%d", message, pgduckdb_state, MyProcPid);
+    elog(INFO, "[con] send_message -> received '%s' | shstate=%p | MyProcPid=%d | MyBackendId=%d | MaxBackends=%d"
+        , message, pgduckdb_state, MyProcPid, MyBackendId, MaxBackends);
     pgduckdb_init_shmem();
 
-    elog(INFO, "[cli] pid -> %d / workerpid = %d / workerlatch = %p",
+    elog(INFO, "[con] pid -> %d / workerpid = %d / workerlatch = %p",
         MyProcPid, pgduckdb_state->worker_pid, pgduckdb_state->worker_latch);
 
     MemoryContext old_context = MemoryContextSwitchTo(TopMemoryContext);
 
-    if (commands_queue == NULL) {
-        elog(INFO, "[cli] send_message -> attaching to seg=%u", pgduckdb_state->seg);
-        dsm_segment *seg = dsm_attach(pgduckdb_state->seg);
-        if (seg == NULL)
-            ereport(ERROR, (errmsg("Failed to attach to shm segment")));
+    if (pgduckdb_internal_state == NULL) {
+        Assert(pgduckdb_state->connection_seg[MyBackendId] == DSM_HANDLE_INVALID);
 
-        shm_toc *toc = shm_toc_attach(PG_DUCKDB_MAGIC, dsm_segment_address(seg));
-
-        {
-            shm_mq* mq = (shm_mq*)shm_toc_lookup(toc, 0, false);
-            shm_mq_set_sender(mq, MyProc);
-            commands_queue = shm_mq_attach(mq, seg, NULL);
-            elog(INFO, "[cli] send_message -> got command queue mq=%p", mq);
-        }
-
-        {
-            shm_mq* mq = (shm_mq*)shm_toc_lookup(toc, 1, false);
-            shm_mq_set_receiver(mq, MyProc);
-            results_queue = shm_mq_attach(mq, seg, NULL);
-            elog(INFO, "[cli] send_message -> got results queue mq=%p", mq);
-        }
-
-        dsm_pin_mapping(seg);
+        elog(INFO, "[con] send_message -> creating new shm segment");
+        pgduckdb_state->connection_seg[MyBackendId] = pgduckdb_create_connection_shm_segment();
+        elog(INFO, "[con] send_message -> pgduckdb_create_connection_shm_segment done for MyBackendId=%u seg=%u; ver=%u",
+            MyBackendId, pgduckdb_state->connection_seg[MyBackendId], pgduckdb_state->state_version);
+        LWLockAcquire(&pgduckdb_state->state_version_lock, LW_EXCLUSIVE);
+        ++pgduckdb_state->state_version; // Increment state version to notify bgw
+        LWLockRelease(&pgduckdb_state->state_version_lock);
+        elog(INFO, "[con] send_message -> incremented version to %u, will notify %p", pgduckdb_state->state_version, pgduckdb_state->worker_latch);
+        SetLatch(pgduckdb_state->worker_latch);
+        elog(INFO, "[con] send_message -> notified bgw, will now wait for bgw to attach");
+        (void) WaitLatch(MyLatch, WL_LATCH_SET | WL_EXIT_ON_PM_DEATH, 0, WAIT_EVENT_MQ_INTERNAL);
+        elog(INFO, "[con] send_message -> bgw attached");
+        ResetLatch(MyLatch); // not strictly necessary, but cleaner.
     }
 
-    shm_mq_result res = shm_mq_send(commands_queue, strlen(message) + 1, message, false, true);
+    shm_mq_result res = shm_mq_send(pgduckdb_internal_state->command_queue, strlen(message) + 1, message, false, true);
     if (res != SHM_MQ_SUCCESS)
         ereport(ERROR, (errmsg("Failed to send message to bgw")));
 
 
     MemoryContextSwitchTo(old_context);
 
-    elog(INFO, "[cli] send_message -> done");
+    elog(INFO, "[con] send_message -> done");
 
     {
         Size received;
         void *data;
-        shm_mq_result res = shm_mq_receive(results_queue, &received, &data, false);
+        shm_mq_result res = shm_mq_receive(pgduckdb_internal_state->results_queue, &received, &data, false);
         if (res == SHM_MQ_SUCCESS) {
-            elog(INFO, "[cli] Received message: %s", (char *) data);
+            elog(INFO, "[con] Received message: %s", (char *) data);
         } else if (res == SHM_MQ_DETACHED) {
-            elog(INFO, "[cli] Message queue detached");
+            elog(INFO, "[con] Message queue detached");
         } else {
-            elog(INFO, "[cli] Message queue error?");
+            elog(INFO, "[con] Message queue error?");
         }
     }
 
@@ -231,13 +236,115 @@ extern void die(SIGNAL_ARGS);
 extern void procsignal_sigusr1_handler(SIGNAL_ARGS);
 extern int * __error(void);
 
+static uint32_t bgw_state_version = 0;
+
+static List* bgw_connections = NULL;
+
+typedef struct {
+    shm_mq_handle *command_queue;
+    shm_mq_handle *results_queue;
+    BackendId backend_id;
+} PGDuckDBConnectionDescriptor;
+
+bool has_connection(BackendId id) {
+    ListCell *lc;
+    foreach(lc, bgw_connections) {
+        PGDuckDBConnectionDescriptor* s = (PGDuckDBConnectionDescriptor*)lfirst(lc);
+        if (s->backend_id == id) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void do_update_state(void) {
+    for (BackendId i = 0; i < MAX_BACKENDS; ++i) {
+        if (pgduckdb_state->connection_seg[i] == DSM_HANDLE_INVALID) {
+            // TODO - remove connection from bgw_connections if it's there
+            continue;
+        }
+
+        if (has_connection(i)) {
+            continue;
+        }
+
+        // Allocate connection descriptors on TopMemoryContext
+        MemoryContext old_context = MemoryContextSwitchTo(TopMemoryContext);
+        PGDuckDBConnectionDescriptor* desc = (PGDuckDBConnectionDescriptor*)palloc0(sizeof(PGDuckDBConnectionDescriptor));
+
+        dsm_segment *seg = dsm_attach(pgduckdb_state->connection_seg[i]);
+        if (seg == NULL) {
+            elog(INFO, "[bgw] Failed to attach to shm segment %u", pgduckdb_state->connection_seg[i]);
+            continue;
+        }
+
+        shm_toc *toc = shm_toc_attach(PG_DUCKDB_MAGIC, dsm_segment_address(seg));
+        PGDuckDBConnectionState* s = (PGDuckDBConnectionState*)shm_toc_lookup(toc, 2, false);
+        if (s == NULL) {
+            elog(INFO, "[bgw] Failed to lookup connection state");
+            continue;
+        }
+
+        Assert(s->backend_id == i);
+        desc->backend_id = i;
+
+        {
+            shm_mq* mq = (shm_mq*)shm_toc_lookup(toc, 0, false);
+            shm_mq_set_receiver(mq, MyProc);
+            desc->command_queue = shm_mq_attach(mq, seg, NULL);
+            elog(INFO, "[bgw] update_state -> got command queue mq=%p", mq);
+        }
+
+        {
+            shm_mq* mq = (shm_mq*)shm_toc_lookup(toc, 1, false);
+            shm_mq_set_sender(mq, MyProc);
+            desc->results_queue = shm_mq_attach(mq, seg, NULL);
+            elog(INFO, "[bgw] update_state -> got results queue mq=%p", mq);
+        }
+
+        bgw_connections = lappend(bgw_connections, desc);
+        SetLatch(s->latch);
+        elog(INFO, "[bgw] Added connection %u", i);
+        MemoryContextSwitchTo(old_context);
+    }
+}
+
+/*
+    Update BGW state from shared memory by iterating over all connection_seg and comparing with our internal list
+    - if connection_seg is not in our list, add it
+    - if connection_seg is in our list but not in shared memory, remove it
+
+    Returns false if state has not changed, true otherwise
+*/
+bool update_state(void) {
+    uint32_t initial_state_version = bgw_state_version;
+    do {
+
+        LWLockAcquire(&pgduckdb_state->state_version_lock, LW_SHARED);
+        uint32_t shared_state_version = pgduckdb_state->state_version;
+        LWLockRelease(&pgduckdb_state->state_version_lock);
+
+        if (bgw_state_version == shared_state_version) {
+            elog(INFO, "[bgw] State is up to date: shared_state_version=%u; initial_state_version=%u", shared_state_version, initial_state_version);
+            // We've now converged with the shared state
+            return initial_state_version != shared_state_version;
+        }
+
+        elog(INFO, "[bgw] Updating state to %u", shared_state_version);
+        do_update_state();
+        bgw_state_version = shared_state_version;
+        elog(INFO, "[bgw] State is now at %u", shared_state_version);
+    } while (true);
+}
+
 PGDLLEXPORT void pgduckdb_bgw_main(PG_FUNCTION_ARGS)
 {
     elog(INFO, "[bgw] Started PGDuckDB BGW");
 
-    pgduckdb_internal_state = (PGDuckDBInternalState *) palloc0(sizeof(PGDuckDBInternalState));
+    pgduckdb_init_shmem();
 
-    pgduckdb_init_shmem(true);
+    pgduckdb_state->worker_latch = MyLatch;
 
     // Set sig handlers
     pqsignal(SIGUSR1, procsignal_sigusr1_handler);
@@ -264,32 +371,41 @@ PGDLLEXPORT void pgduckdb_bgw_main(PG_FUNCTION_ARGS)
                         ALLOCSET_DEFAULT_SIZES);
         oldctx = MemoryContextSwitchTo(subctx);
 
-        elog(INFO, "[bgw] Ready to do some work! - here");
+        elog(INFO, "[bgw] Ready to do some work!");
 
         shm_mq_result res;
         Size received;
         void *data;
 
         ResetLatch(MyLatch);
-        (void) WaitLatch(MyLatch, WL_LATCH_SET | WL_EXIT_ON_PM_DEATH, 0,
-                         WAIT_EVENT_MQ_INTERNAL);
+        (void) WaitLatch(MyLatch, WL_LATCH_SET | WL_EXIT_ON_PM_DEATH, 0, WAIT_EVENT_MQ_INTERNAL);
 
-        elog(INFO, "[bgw] Latch set! - there");
+        elog(INFO, "[bgw] Latch set!");
 
-        res = shm_mq_receive(pgduckdb_internal_state->input_queue, &received, &data, false);
-        if (res == SHM_MQ_SUCCESS) {
-            char *message = (char *) data;
-            elog(INFO, "[bgw] Received message: %s", message);
+        update_state();
 
-            shm_mq_result res = shm_mq_send(pgduckdb_internal_state->output_queue, strlen(message) + 1, message, false, true);
-            if (res != SHM_MQ_SUCCESS)
-                ereport(ERROR, (errmsg("Failed to send message back to cli")));
+        ListCell *lc;
+        foreach(lc, bgw_connections) {
+            PGDuckDBConnectionDescriptor* s = (PGDuckDBConnectionDescriptor*)lfirst(lc);
+            elog(INFO, "[bgw] Receiving from connection %u", s->backend_id);
 
-        } else if (res == SHM_MQ_DETACHED) {
-            elog(INFO, "[bgw] Message queue detached");
-            break;
-        } else {
-            elog(INFO, "[bgw] Message queue error?");
+            res = shm_mq_receive(s->command_queue, &received, &data, true);
+            if (res == SHM_MQ_SUCCESS) {
+                char *message = (char *) data;
+                elog(INFO, "[bgw] Received message: %s", message);
+
+                shm_mq_result res = shm_mq_send(s->results_queue, strlen(message) + 1, message, false, true);
+                if (res != SHM_MQ_SUCCESS)
+                    ereport(ERROR, (errmsg("Failed to send message back to cli")));
+
+            } else if (res == SHM_MQ_DETACHED) {
+                elog(INFO, "[bgw] Message queue detached");
+                break;
+            } else if (res == SHM_MQ_WOULD_BLOCK) {
+                elog(INFO, "[bgw] Message queue would block");
+            } else {
+                elog(INFO, "[bgw] Message queue error?");
+            }
         }
 
         /* Switch back to original memory context. */
