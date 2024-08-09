6e9dbe4540fb1850dae92c6ca20c862d15050112
Jelte Fennema-Nio <github-tech@jeltef.nl>
2024-08-09 10:41:23 +0200

    Revert "Move httpfs extension outside of duckdb library. (#92)"
    This reverts commit 11b181f6d273ce58deb8fe126cab2fcc7a77d7dd.

--- third_party/pg_duckdb_extensions.cmake
+++
@@ -1,6 +0,0 @@
-duckdb_extension_load(json)
-# This is relative to third_party/duckdb dir
-duckdb_extension_load(cached_httpfs
-    SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/../cached_httpfs"
-    INCLUDE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/../cached_httpfs/include"
-)
