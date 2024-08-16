8a9a74f490cdf7c224ecccf8e907eb56131be288
Jelte Fennema-Nio <github-tech@jeltef.nl>
2024-08-16 16:15:36 +0200

    Update tests now that "AS (a int)" doesn't work anymore

--- test/regression/sql/duckdb_only_functions.sql
+++ test/regression/sql/duckdb_only_functions.sql
@@ -4,10 +4,10 @@ SET duckdb.execution = false;
 
 \getenv pwd PWD
 
-select * from read_parquet(:'pwd' || '/data/unsigned_types.parquet') as (a int);
-select * from read_parquet(ARRAY[:'pwd' || '/data/unsigned_types.parquet']) as (a int);
+select * from read_parquet(:'pwd' || '/data/unsigned_types.parquet') as (usmallint int);
+select * from read_parquet(ARRAY[:'pwd' || '/data/unsigned_types.parquet']) as (usmallint int);
 
-select * from read_csv(:'pwd' || '/data/web_page.csv') as (a int) LIMIT 2;
-select * from read_csv(ARRAY[:'pwd' || '/data/web_page.csv']) as (a int) LIMIT 2;
+select * from read_csv(:'pwd' || '/data/web_page.csv') as (column00 int) LIMIT 2;
+select * from read_csv(ARRAY[:'pwd' || '/data/web_page.csv']) as (column00 int) LIMIT 2;
 
 -- TODO: Add a test for scan_iceberg once we have a test table
