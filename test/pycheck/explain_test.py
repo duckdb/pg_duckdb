e667ec73b42c39b91dfcf0282d8153d86ef3b0c1
Jelte Fennema-Nio <github-tech@jeltef.nl>
2024-09-17 13:25:09 +0200

    Test extended explain queries

--- test/pycheck/explain_test.py
+++ test/pycheck/explain_test.py
@@ -2,7 +2,7 @@ from .utils import Cursor
 
 
 def test_explain(cur: Cursor):
-    cur.sql("CREATE TABLE test_table (id int primary key, name text)")
+    cur.sql("CREATE TABLE test_table (id int, name text)")
     result = cur.sql("EXPLAIN SELECT count(*) FROM test_table")
     plan = "\n".join(result)
     assert "UNGROUPED_AGGREGATE" in plan
@@ -12,4 +12,18 @@ def test_explain(cur: Cursor):
     plan = "\n".join(result)
     assert "Query Profiling Information" in plan
     assert "UNGROUPED_AGGREGATE" in plan
+    assert "Total Time:" in plan
+
+    result = cur.sql("EXPLAIN SELECT count(*) FROM test_table where id = %s", (1,))
+    plan = "\n".join(result)
+    assert "UNGROUPED_AGGREGATE" in plan
+    assert "id=1 AND id IS NOT NULL" in plan
+    assert "Total Time:" not in plan
+
+    result = cur.sql(
+        "EXPLAIN ANALYZE SELECT count(*) FROM test_table where id = %s", (1,)
+    )
+    plan = "\n".join(result)
+    assert "UNGROUPED_AGGREGATE" in plan
+    assert "id=1 AND id IS NOT NULL" in plan
     assert "Total Time:" in plan
