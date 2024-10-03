f73517fd45c6357fa49366bf8d140fee26c10712
mkaruza <mkaruza@users.noreply.github.com>
2024-10-03 14:43:39 +0200

    Review changes

--- test/regression/sql/altered_tables.sql
+++ test/regression/sql/altered_tables.sql
@@ -8,14 +8,15 @@ ALTER TABLE table_missing_attrs ADD COLUMN a int DEFAULT 10;
 ALTER TABLE table_missing_attrs ADD COLUMN b int DEFAULT 20;
 ALTER TABLE table_missing_attrs ADD COLUMN c int DEFAULT NULL;
 ALTER TABLE table_missing_attrs ADD COLUMN d int DEFAULT 30;
+ALTER TABLE table_missing_attrs ADD COLUMN e int DEFAULT 0;
 
 SELECT * FROM table_missing_attrs;
-SELECT a, c, d id FROM table_missing_attrs;
+SELECT a, c, d, e FROM table_missing_attrs;
 
 INSERT INTO table_missing_attrs(id, a, b) VALUES (2, 100, 200);
 ALTER TABLE table_missing_attrs ADD COLUMN f TEXT DEFAULT 'abcdefghijklmnopqrstuvwxyz';
 
 SELECT * FROM table_missing_attrs;
-SELECT a, c, d, f id FROM table_missing_attrs;
+SELECT a, c, d, f FROM table_missing_attrs;
 
 DROP TABLE table_missing_attrs;
