01f3050ee3d5f19e31d4a8031fc19c7a99fe0fe5
Y. <y.lemaout@gmail.com>
2024-10-04 14:39:21 +0200

    Call `GetActiveSnapshot` when creating TX (#246)
    Co-authored-by: Jelte Fennema-Nio <1162278+JelteF@users.noreply.github.com>

--- include/pgduckdb/catalog/pgduckdb_storage.hpp
+++ include/pgduckdb/catalog/pgduckdb_storage.hpp
@@ -10,18 +10,9 @@ extern "C" {
 
 namespace duckdb {
 
-class PostgresStorageExtensionInfo : public StorageExtensionInfo {
-public:
-	PostgresStorageExtensionInfo(Snapshot snapshot) : snapshot(snapshot) {
-	}
-
-public:
-	Snapshot snapshot;
-};
-
 class PostgresStorageExtension : public StorageExtension {
 public:
-	PostgresStorageExtension(Snapshot snapshot);
+	PostgresStorageExtension();
 };
 
 } // namespace duckdb
--- src/catalog/pgduckdb_transaction_manager.cpp
+++ src/catalog/pgduckdb_transaction_manager.cpp
@@ -1,16 +1,16 @@
 #include "pgduckdb/catalog/pgduckdb_transaction_manager.hpp"
 #include "duckdb/main/attached_database.hpp"
+#include "pgduckdb/pgduckdb_process_lock.hpp"
 
 namespace duckdb {
 
-PostgresTransactionManager::PostgresTransactionManager(AttachedDatabase &db_p, PostgresCatalog &catalog,
-                                                       Snapshot snapshot)
-    : TransactionManager(db_p), catalog(catalog), snapshot(snapshot) {
+PostgresTransactionManager::PostgresTransactionManager(AttachedDatabase &db_p, PostgresCatalog &catalog)
+    : TransactionManager(db_p), catalog(catalog) {
 }
 
 Transaction &
 PostgresTransactionManager::StartTransaction(ClientContext &context) {
-	auto transaction = make_uniq<PostgresTransaction>(*this, context, catalog, snapshot);
+	auto transaction = make_uniq<PostgresTransaction>(*this, context, catalog, GetActiveSnapshot());
 	auto &result = *transaction;
 	lock_guard<mutex> l(transaction_lock);
 	transactions[result] = std::move(transaction);
