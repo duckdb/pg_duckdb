f6ec22ba93364272ea7cab0e0071833f35c27801
Yves <yves@motherduck.com>
2024-10-18 17:12:44 +0200

    Ignore jemalloc extension

--- test/regression/sql/extensions.sql
+++ test/regression/sql/extensions.sql
@@ -1,7 +1,7 @@
 
 SET duckdb.execution TO false;
 
-SELECT * FROM duckdb.raw_query($$ SELECT extension_name, loaded, installed FROM duckdb_extensions() WHERE loaded $$);
+SELECT * FROM duckdb.raw_query($$ SELECT extension_name, loaded, installed FROM duckdb_extensions() WHERE loaded and extension_name != 'jemalloc' $$);
 
 SELECT last_value FROM duckdb.extensions_table_seq;
 
@@ -11,13 +11,13 @@ INSERT INTO duckdb.extensions (name, enabled) VALUES ('icu', TRUE);
 
 SELECT last_value FROM duckdb.extensions_table_seq;
 
-SELECT * FROM duckdb.raw_query($$ SELECT extension_name, loaded, installed FROM duckdb_extensions() WHERE loaded $$);
+SELECT * FROM duckdb.raw_query($$ SELECT extension_name, loaded, installed FROM duckdb_extensions() WHERE loaded and extension_name != 'jemalloc' $$);
 
 INSERT INTO duckdb.extensions (name, enabled) VALUES ('aws', TRUE);
 
 SELECT last_value FROM duckdb.extensions_table_seq;
 
-SELECT * FROM duckdb.raw_query($$ SELECT extension_name, loaded, installed FROM duckdb_extensions() WHERE loaded $$);
+SELECT * FROM duckdb.raw_query($$ SELECT extension_name, loaded, installed FROM duckdb_extensions() WHERE loaded and extension_name != 'jemalloc' $$);
 
 -- DELETE SHOULD TRIGGER UPDATE OF EXTENSIONS
 -- But we do not unload for now (would require a restart of DuckDB)
@@ -25,6 +25,6 @@ DELETE FROM duckdb.extensions WHERE name = 'aws';
 
 SELECT last_value FROM duckdb.extensions_table_seq;
 
-SELECT * FROM duckdb.raw_query($$ SELECT extension_name, loaded, installed FROM duckdb_extensions() WHERE loaded $$);
+SELECT * FROM duckdb.raw_query($$ SELECT extension_name, loaded, installed FROM duckdb_extensions() WHERE loaded and extension_name != 'jemalloc' $$);
 
 SET duckdb.execution TO true;
