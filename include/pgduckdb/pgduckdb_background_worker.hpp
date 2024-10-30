82d923a863a5cb7c7993aa1118e550f0b77439f2
Y. <y.lemaout@gmail.com>
2024-10-30 16:38:12 +0200

    Remove unused headers (#372)

--- include/pgduckdb/pgduckdb_background_worker.hpp
+++ include/pgduckdb/pgduckdb_background_worker.hpp
@@ -1,20 +1,5 @@
 #pragma once
 
-#include "duckdb.hpp"
-#include "duckdb/parser/parsed_data/create_info.hpp"
-#include "duckdb/parser/parsed_data/create_info.hpp"
-#include "duckdb/common/unordered_set.hpp"
-#include "duckdb/parser/column_definition.hpp"
-#include "duckdb/parser/constraint.hpp"
-#include "duckdb/parser/statement/select_statement.hpp"
-#include "duckdb/catalog/catalog_entry/column_dependency_manager.hpp"
-#include "duckdb/parser/column_list.hpp"
-#include "duckdb/parser/parsed_data/create_table_info.hpp"
-
-extern "C" {
-#include "postgres.h"
-}
-
 void DuckdbInitBackgroundWorker(void);
 
 namespace pgduckdb {
