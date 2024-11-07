3c915622601b15110f50a323876895e9f3f43402
mkaruza <mkaruza@users.noreply.github.com>
2024-11-07 13:18:31 +0100

    Regression test for iceberg dataset (#404)
    Added iceberg dataset (from https://duckdb.org/docs/extensions/iceberg)
    and added regression test for iceberg functions.

--- test/regression/sql/read_functions.sql
+++ test/regression/sql/read_functions.sql
@@ -30,3 +30,29 @@ SELECT duckdb.install_extension('delta');
 
 SELECT count(a) FROM delta_scan('../../data/delta_table') AS (a INT);
 SELECT * FROM delta_scan('../../data/delta_table') AS (a INT, b VARCHAR) WHERE (a = 1 OR b = 'delta_table_3');
+
+
+-- iceberg_*
+
+SELECT duckdb.install_extension('iceberg');
+
+SELECT COUNT(l_orderkey) FROM iceberg_scan('../../data/lineitem_iceberg', allow_moved_paths => true) AS (l_orderkey BIGINT);
+
+-- TPCH query #6
+SELECT
+	sum(l_extendedprice * l_discount) as revenue
+FROM
+	iceberg_scan('../../data/lineitem_iceberg', allow_moved_paths => true) AS
+        (l_extendedprice DOUBLE PRECISION,
+         l_discount DOUBLE PRECISION,
+         l_shipdate DATE,
+         l_quantity DOUBLE PRECISION)
+WHERE
+	l_shipdate >= date '1997-01-01'
+	AND l_shipdate < date '1997-01-01' + interval '1' year
+	AND l_discount between 0.08 - 0.01 and 0.08 + 0.01
+	AND l_quantity < 25
+LIMIT 1;
+
+SELECT * FROM iceberg_snapshots('../../data/lineitem_iceberg');
+SELECT * FROM iceberg_metadata('../../data/lineitem_iceberg',  allow_moved_paths => true);
