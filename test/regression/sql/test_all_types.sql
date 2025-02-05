ba4fc366ba0c688e37dd8889026f75848c0188c9
Jelte Fennema-Nio <github-tech@jeltef.nl>
2025-02-05 12:27:04 +0100

    Add test that runs DuckDB its test_all_types function
    This is an easy way to test conversion from Postgres to DuckDB for types
    that don't have an equivalent in Postgres.
    
    It also gives us an easy todo list of types that we don't support yet.

---
+++ test/regression/sql/test_all_types.sql
@@ -0,0 +1,30 @@
+\x on
+SET bytea_output = 'escape';
+SELECT * FROM duckdb.query($$
+FROM test_all_types()
+SELECT * exclude(
+    tinyint, -- PG14 outputs this differently currently
+    varint,
+    TIME,
+    time_tz,
+    bit,
+    small_enum,
+    medium_enum,
+    large_enum,
+    timestamptz_array,
+    struct,
+    struct_of_arrays,
+    array_of_structs,
+    map,
+    "union",
+    fixed_int_array,
+    fixed_varchar_array,
+    fixed_nested_int_array,
+    fixed_nested_varchar_array,
+    fixed_struct_array,
+    struct_of_fixed_array,
+    fixed_array_of_int_list,
+    list_of_fixed_int_array,
+    nested_int_array, -- The nested array has different lengths, which is not possible in PG
+)
+$$)
