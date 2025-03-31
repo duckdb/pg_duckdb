1c3631c8f14496fea63689d8c3ee0b922685da17
dentiny <dentinyhao@gmail.com>
2025-03-31 08:57:00 +0000

    type support test

--- test/regression/sql/type_support.sql
+++ test/regression/sql/type_support.sql
@@ -58,9 +58,15 @@ CREATE TABLE varbit_tbl(a VARBIT);
 -- Insert a few kinds of bitstrings: (1) less than 8 bits; (2) equal to 8 bits; (3) larger than 8 bits.
 INSERT INTO varbit_tbl SELECT CAST(a AS VARBIT) FROM (VALUES (B'1010'::VARBIT), (B'10100011'::VARBIT), (B'1010001011'::VARBIT), (NULL)) t(a);
 SELECT * FROM varbit_tbl;
-SELECT * FROM varbit_tbl WHERE a = B'1010'::VARBIT;
-SELECT * FROM varbit_tbl WHERE a = B'10100011'::VARBIT;
-SELECT * FROM varbit_tbl WHERE a = B'1010001011'::VARBIT;
+
+-- BIT ARRAY
+CREATE TABLE fixed_var_tbl(a VARBIT);
+INSERT INTO fixed_var_tbl SELECT CAST(a AS BIT(4)) FROM (VALUES (B'101'), (NULL)) t(a);
+SELECT * FROM fixed_var_tbl;
+
+CREATE TABLE varying_var_tbl(a BIT VARYING(5));
+INSERT INTO varying_var_tbl SELECT CAST(a AS BIT VARYING(5)) FROM (VALUES (B'101'), (NULL)) t(a);
+SELECT * FROM varying_var_tbl;
 
 -- TIME
 CREATE TABLE time_tbl(a TIME);
@@ -222,6 +228,9 @@ DROP TABLE varchar_tbl;
 DROP TABLE text_tbl;
 DROP TABLE date_tbl;
 DROP TABLE interval_tbl;
+DROP TABLE varbit_tbl;
+DROP TABLE fixed_var_tbl;
+DROP TABLE varying_var_tbl;
 DROP TABLE time_tbl;
 DROP TABLE timestamp_tbl;
 DROP TABLE timestamptz_tbl;
