34d99acfd6933c1ab5d6003b912c1c94412aac91
Yves <yves@motherduck.com>
2025-04-15 15:15:38 +0200

    Only fetch user own secrets

--- src/pgduckdb_secrets_helper.cpp
+++ src/pgduckdb_secrets_helper.cpp
@@ -96,63 +96,35 @@ List *
 ListDuckDBCreateSecretQueries() {
 	MemoryContext entry_ctx = CurrentMemoryContext;
 	SPI_connect();
+
+	// List all SERVER created with 'duckdb' FDW
 	auto query = R"(
 		SELECT
-			fs.oid as server_oid,
-			fs.srvtype as server_type,
-			um.umuser as um_user_oid
+			fs.oid as server_oid
 		FROM pg_foreign_server fs
 		INNER JOIN pg_foreign_data_wrapper fdw ON fdw.oid = fs.srvfdw
-		LEFT JOIN pg_user_mapping um ON um.umserver = fs.oid
 		WHERE fdw.fdwname = 'duckdb' AND fs.srvtype != 'motherduck';
 	)";
-
-	// Escalate privileges to superuser to read the pg_user_mapping table
-	// XXX: Let's rediscuss this point to make sure we want to share secrets like this across users.
-	Oid saved_userid;
-	int sec_context;
-	GetUserIdAndSecContext(&saved_userid, &sec_context);
-	SetUserIdAndSecContext(BOOTSTRAP_SUPERUSERID, sec_context | SECURITY_LOCAL_USERID_CHANGE);
 
 	auto ret = SPI_exec(query, 0);
 	if (ret != SPI_OK_SELECT) {
 		elog(ERROR, "Can't list DuckDB secrets: %s", SPI_result_code_string(ret));
 	}
 
-	SetUserIdAndSecContext(saved_userid, sec_context);
-
 	List *results = NIL;
 
 	for (uint64_t i = 0; i < SPI_processed; ++i) {
 		HeapTuple tuple = SPI_tuptable->vals[i];
 		bool is_serveroid_null = false;
-		bool is_umoid_null = false;
-		bool is_servertype_null = false;
 		Datum server_oid_datum = SPI_getbinval(tuple, SPI_tuptable->tupdesc, 1, &is_serveroid_null);
-		Datum server_type_datum = SPI_getbinval(tuple, SPI_tuptable->tupdesc, 2, &is_servertype_null);
-		Datum um_user_oid_datum = SPI_getbinval(tuple, SPI_tuptable->tupdesc, 3, &is_umoid_null);
-
 		if (is_serveroid_null) {
 			elog(ERROR, "FATAL: Expected server oid to be returned, but found NULL");
-		} else if (is_servertype_null) {
-			elog(ERROR, "FATAL: Expected server type to be returned, but found NULL");
 		}
 
 		// Get USER MAPPING if it exists
 		const Oid server_oid = DatumGetObjectId(server_oid_datum);
-		const char *type = TextDatumGetCString(server_type_datum);
-		Oid um_user_oid = InvalidOid;
-		List *user_mapping_options = nullptr;
-		if (!is_umoid_null) {
-			um_user_oid = DatumGetObjectId(um_user_oid_datum);
-			auto user_mapping = FindUserMapping(um_user_oid, server_oid, true);
-			if (user_mapping == nullptr) {
-				// Concurrently deleted? Should we run this under locks?
-				elog(ERROR, "FATAL: could not find USER MAPPING for user %u and server %u", um_user_oid, server_oid);
-			}
-
-			user_mapping_options = user_mapping->options;
-		}
+		auto user_mapping = FindUserMapping(GetUserId(), server_oid, true);
+		List *user_mapping_options = user_mapping ? user_mapping->options : NIL;
 
 		// Get SERVER
 		auto server = GetForeignServer(server_oid);
@@ -163,8 +135,8 @@ ListDuckDBCreateSecretQueries() {
 		{
 			// Enter memory context preceding the SPI call so that it survives `SPI_finish`
 			MemoryContext spi_mem_ctx = MemoryContextSwitchTo(entry_ctx);
-			auto name = server->servername;
-			auto secret_query = MakeDuckDBCreateSecretQuery(name, type, server->options, user_mapping_options);
+			auto secret_query = MakeDuckDBCreateSecretQuery(server->servername, server->servertype, server->options,
+			                                                user_mapping_options);
 			results = lappend(results, makeString(secret_query));
 			MemoryContextSwitchTo(spi_mem_ctx);
 		}
@@ -176,13 +148,17 @@ ListDuckDBCreateSecretQueries() {
 
 /*
 Modified version of Postgres' GetUserMapping:
-- doesn't check in the PUBLIC namespace
 - returns nullptr if it doesn't exist (doesn't throw an error)
 - optionally fetches the options
 */
 UserMapping *
 FindUserMapping(Oid userid, Oid serverid, bool with_options) {
 	HeapTuple tp = SearchSysCache2(USERMAPPINGUSERSERVER, ObjectIdGetDatum(userid), ObjectIdGetDatum(serverid));
+	if (!HeapTupleIsValid(tp)) {
+		/* Not found for the specific user -- try PUBLIC */
+		tp = SearchSysCache2(USERMAPPINGUSERSERVER, ObjectIdGetDatum(InvalidOid), ObjectIdGetDatum(serverid));
+	}
+
 	if (!HeapTupleIsValid(tp)) {
 		return nullptr;
 	}
