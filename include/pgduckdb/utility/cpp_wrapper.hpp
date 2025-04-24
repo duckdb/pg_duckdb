b82a24e51f266d5766b363792a9cebb2c653560e
Y. <y.lemaout@gmail.com>
2025-04-24 16:03:11 +0200

    Best effort handling of nested PG_TRY in C++ try (#737)

--- include/pgduckdb/utility/cpp_wrapper.hpp
+++ include/pgduckdb/utility/cpp_wrapper.hpp
@@ -12,6 +12,7 @@ template <typename Func, Func func, typename... FuncArgs>
 typename std::invoke_result<Func, FuncArgs &...>::type
 __CPPFunctionGuard__(const char *func_name, FuncArgs &...args) {
 	const char *error_message = nullptr;
+	auto pg_es_start = PG_exception_stack;
 	try {
 		return func(args...);
 	} catch (duckdb::Exception &ex) {
@@ -25,6 +26,51 @@ __CPPFunctionGuard__(const char *func_name, FuncArgs &...args) {
 		} else {
 			error_message = pstrdup(ex.what());
 		}
+	}
+
+	// This can happen if `func` or one of the function it calls uses `PG_TRY`
+	// If we are here, it means we got out of the PG_TRY block through a C++ exception
+	// without running the corresponding `PG_CATCH` block.
+	//
+	// In that case, the `PG_exception_stack` points to an invalid stack and calling
+	// `elog(ERROR, ...)` below would lead to a crash when we finally hit `PG_CATCH`
+	//
+	// As a best effort, we reset the `PG_exception_stack` to the value we had
+	// entering the wrapper, but this might leak other resources that were supposed
+	// to be closed in the missed `PG_CATCH`
+	//
+	// For developers trying to fix this:
+	// you need to make sure that `func` can never throw a C++ excepction
+	// from within a `PG_TRY` block.
+
+	// Example of problematic code:
+	// void my_func() {
+	//   PG_TRY(); {
+	//     throw duckdb::Exception("foo");
+	//   } PG_CATCH(); {
+	//    // This PG_CATCH block will never be executed
+	//   } PG_END_TRY();
+	// }
+
+	// In this case the `PG_CATCH` block that will handle the error thrown below
+	// would try to reset the stack to the begining of `my_func` and crash
+	//
+	// So instead this should also be wrapped in a `InvokeCPPFunc` like:
+	//
+	// void my_throwing_func() {
+	//  throw duckdb::Exception("foo");
+	// }
+	//
+	// void my_func() {
+	//   PG_TRY(); {
+	//     InvokeCPPFunc(my_throwing_func);
+	//   } PG_CATCH(); {
+	//    // This PG_CATCH block will now be executed
+	//   } PG_END_TRY();
+	// }
+	if (pg_es_start != PG_exception_stack) {
+		elog(WARNING, "WARNING: Unexpected exception stack pointer. This is not expected, please report this.");
+		PG_exception_stack = pg_es_start;
 	}
 
 	elog(ERROR, "(PGDuckDB/%s) %s", func_name, error_message);
