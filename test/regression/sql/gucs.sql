f1fc8f4747e37daf0f9a23f26a50e46b287726fa
Yves <yves@motherduck.com>
2025-04-25 14:24:56 +0200

    Add more tests

--- test/regression/sql/gucs.sql
+++ test/regression/sql/gucs.sql
@@ -11,3 +11,7 @@ select * from duckdb.query($$ select current_setting('memory_limit') != '3.7 GiB
 
 -- Don't recycle DuckDB, make sure we get an error:
 set duckdb.memory_limit = '1GiB';
+
+set duckdb.threads = 42;
+
+set duckdb.autoinstall_known_extensions = false;
