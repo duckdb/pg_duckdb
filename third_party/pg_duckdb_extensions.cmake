53b948fa87ef2e3f670c2b03751fbcdcd371eb58
Jelte Fennema-Nio <github-tech@jeltef.nl>
2025-05-30 10:52:18 +0200

    Bump httpfs extension

--- third_party/pg_duckdb_extensions.cmake
+++ third_party/pg_duckdb_extensions.cmake
@@ -2,6 +2,6 @@ duckdb_extension_load(json)
 duckdb_extension_load(icu)
 duckdb_extension_load(httpfs
     GIT_URL https://github.com/duckdb/duckdb-httpfs
-    GIT_TAG 217ec8e04f6ed419c866a6d2496aa15aace4382f
+    GIT_TAG 7ce5308ed8fe48b593538dbd54344a2fc0695bc7
     INCLUDE_DIR extension/httpfs/include
 )
