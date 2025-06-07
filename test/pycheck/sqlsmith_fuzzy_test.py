0ff332f6a9931f3ee623ff1e41d71ed50ce8fa41
0xgouda <gouda0x@gmail.com>
2025-06-07 20:45:34 +0300

    Use generate_series() to generate dummy data for sqlsmith

--- test/pycheck/sqlsmith_fuzzy_test.py
+++ test/pycheck/sqlsmith_fuzzy_test.py
@@ -7,10 +7,15 @@ def test_fuzzy_pg_crashing(pg: Postgres, cur: Cursor):
     cur.sql("CREATE DATABASE sqlsmith_test")
     with pg.cur(dbname="sqlsmith_test") as sqlsmith_cur:
         sqlsmith_cur.sql("CREATE EXTENSION pg_duckdb")
-        # create a test table with arbitary data to allow sqlsmith to do more operations
+        # create test tables with dummy data to allow sqlsmith to do more operations
         sqlsmith_cur.sql("CREATE TABLE test_table (id INT, name TEXT)")
+        sqlsmith_cur.sql("CREATE TABLE test_table2 (id INT, name VARCHAR)")
+
         sqlsmith_cur.sql(
-            "INSERT INTO test_table (id, name) VALUES (1, 'Alice'), (2, 'Bob')"
+            "INSERT INTO test_table (SELECT num, 'name_' || num FROM generate_series(1, 100) as num)"
+        )
+        sqlsmith_cur.sql(
+            "INSERT INTO test_table2 (SELECT num, 'name_' || num FROM generate_series(10, 100) as num)"
         )
 
     # any crash caused by sqlsmith will be detected from logs on the teardown of pg fixture
