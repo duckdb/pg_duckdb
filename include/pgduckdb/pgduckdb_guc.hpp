12dee2e43520144059f007d37ce4863b9c879d31
Jelte Fennema-Nio <jelte@motherduck.com>
2025-06-16 17:41:32 +0200

    Support MotherDuck views (#822)
    This adds support for creating, querying and syncing VIEWs from
    MotherDuck.
    
    Views created by `CREATE VIEW` get created in MotherDuck if one of the
    following conditions is met:
    
    1. The view is created in a `ddb$` schema.
    2. The query of the view accesses DuckDB tables or DuckDB views.
    3. `duckdb.force_motherduck_views` is set to `true`.
    
    Fixes #474

--- include/pgduckdb/pgduckdb_guc.hpp
+++ include/pgduckdb/pgduckdb_guc.hpp
@@ -19,6 +19,7 @@ extern int duckdb_threads_for_postgres_scan;
 extern int duckdb_max_workers_per_postgres_scan;
 extern char *duckdb_postgres_role;
 extern char *duckdb_motherduck_session_hint;
+extern bool duckdb_force_motherduck_views;
 extern char *duckdb_temporary_directory;
 extern char *duckdb_extension_directory;
 extern char *duckdb_max_temp_directory_size;
--- include/pgduckdb/pgduckdb_ruleutils.h
+++ include/pgduckdb/pgduckdb_ruleutils.h
@@ -13,7 +13,7 @@ char *pgduckdb_function_name(Oid function_oid, bool *use_variadic_p);
 char *pgduckdb_get_querydef(Query *);
 char *pgduckdb_get_tabledef(Oid relation_id);
 char *pgduckdb_get_alter_tabledef(Oid relation_oid, AlterTableStmt *alter_stmt);
-char *pgduckdb_get_rename_tabledef(Oid relation_oid, RenameStmt *rename_stmt);
+char *pgduckdb_get_rename_relationdef(Oid relation_oid, RenameStmt *rename_stmt);
 bool pgduckdb_is_not_default_expr(Node *node, void *context);
 List *pgduckdb_db_and_schema(const char *postgres_schema_name, bool is_duckdb_table);
 const char *pgduckdb_db_and_schema_string(const char *postgres_schema_name, bool is_duckdb_table);
@@ -24,6 +24,7 @@ bool pgduckdb_var_is_duckdb_row(Var *var);
 bool pgduckdb_func_returns_duckdb_row(RangeTblFunction *rtfunc);
 Var *pgduckdb_duckdb_row_subscript_var(Expr *expr);
 bool pgduckdb_reconstruct_star_step(StarReconstructionContext *ctx, ListCell *tle_cell);
+bool pgduckdb_replace_subquery_with_view(Query *query, StringInfo buf);
 int pgduckdb_show_type(Const *constval, int original_showtype);
 bool pgduckdb_subscript_has_custom_alias(Plan *plan, List *rtable, Var *subscript_var, char *colname);
 SubscriptingRef *pgduckdb_strip_first_subscript(SubscriptingRef *sbsref, StringInfo buf);
@@ -31,5 +32,7 @@ char *pgduckdb_write_row_refname(StringInfo buf, char *refname, bool is_top_leve
 bool is_system_sampling(const char *tsm_name, int num_args);
 bool is_bernoulli_sampling(const char *tsm_name, int num_args);
 void pgduckdb_add_tablesample_percent(const char *tsm_name, StringInfo buf, int num_args);
+char *pgduckdb_get_viewdef(const ViewStmt *stmt, const char *postgres_schema_name, const char *view_name,
+                           const char *duckdb_query_string);
 
 extern bool outermost_query;
