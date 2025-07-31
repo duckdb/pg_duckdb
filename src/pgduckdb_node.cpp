ca8c1d9f7ce8b4d1012b7e54b6374724e64969f9
Yuwei Xiao <qianzhen@leadincloud.com>
2025-07-31 11:00:40 +0800

    fix format

--- src/pgduckdb_node.cpp
+++ src/pgduckdb_node.cpp
@@ -224,7 +224,7 @@ ExecuteQuery(DuckdbScanState *state) {
 					execution_result = pending->ExecuteTask();
 				} while (execution_result != duckdb::PendingExecutionResult::EXECUTION_ERROR &&
 				         execution_result != duckdb::PendingExecutionResult::NO_TASKS_AVAILABLE &&
-						 execution_result != duckdb::PendingExecutionResult::EXECUTION_FINISHED);
+				         execution_result != duckdb::PendingExecutionResult::EXECUTION_FINISHED);
 
 				pending->Close();
 			} catch (std::exception &ex) {
