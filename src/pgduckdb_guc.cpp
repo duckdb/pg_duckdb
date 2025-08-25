c8f42844ac99556e5ca20a862a33137781db96e1
Jelte Fennema-Nio <jelte@motherduck.com>
2025-08-25 14:54:31 +0200

    Use empty collation by default
    Specifying `C` or `BINARY` as the `default_collation` causes duckdb to
    disable various optimizations. Arguably that's a bug, but to work around
    that, we now leave the default empty.

--- src/pgduckdb_guc.cpp
+++ src/pgduckdb_guc.cpp
@@ -136,7 +136,7 @@ bool duckdb_autoload_known_extensions = true;
 char *duckdb_temporary_directory = MakeDirName("temp");
 char *duckdb_extension_directory = MakeDirName("extensions");
 char *duckdb_max_temp_directory_size = strdup("");
-char *duckdb_default_collation = strdup("C");
+char *duckdb_default_collation = strdup("");
 
 void
 InitGUC() {
