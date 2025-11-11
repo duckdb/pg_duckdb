8447898e16ff2de6487d54c3b61403278ec7f59b
Jelte Fennema-Nio <jelte@motherduck.com>
2025-11-11 15:04:06 +0100

    Improve test

--- test/regression/sql/duckdb_views.sql
+++ test/regression/sql/duckdb_views.sql
@@ -1,16 +1,25 @@
 CREATE USER duckdb_view_admin IN ROLE duckdb_group;
-CREATE USER duckdb_view_user1;
+GRANT ALL ON SCHEMA public TO duckdb_view_admin;
+CREATE USER duckdb_view_user1 IN ROLE duckdb_group;
 
 SET ROLE duckdb_view_admin;
 
-CREATE VIEW duckdb_view AS SELECT * FROM read_parquet('/nonexisting.parquet');
+CREATE VIEW duckdb_view AS SELECT r['a']::int as a FROM duckdb.query($$ SELECT 1 a $$) r;
+CREATE VIEW postgres_view AS SELECT 2 from generate_series(1,2);
 
 SET ROLE duckdb_view_user1;
 
-SELECT * from duckdb_view LIMIT 1;
+SELECT * from duckdb_view;
+SELECT * from postgres_view;
+SELECT * from duckdb.query($$ FROM pgduckdb.public.duckdb_view $$);
+SELECT * from duckdb.query($$ FROM pgduckdb.public.postgres_view $$);
 
 SET ROLE duckdb_view_admin;
 GRANT SELECT ON duckdb_view TO duckdb_view_user1;
+GRANT SELECT ON postgres_view TO duckdb_view_user1;
 SET ROLE duckdb_view_user1;
 
-SELECT * from duckdb_view LIMIT 1;
+SELECT * from duckdb_view;
+SELECT * from postgres_view;
+SELECT * from duckdb.query($$ FROM pgduckdb.public.duckdb_view $$);
+SELECT * from duckdb.query($$ FROM pgduckdb.public.postgres_view $$);
