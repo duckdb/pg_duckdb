b974754461f14e089dc36974b30273957e27180c
Jelte Fennema-Nio <jelte@motherduck.com>
2025-11-14 14:43:52 +0100

    Fix compilation errors

--- third_party/pg_duckdb_extensions.cmake
+++ third_party/pg_duckdb_extensions.cmake
@@ -2,5 +2,5 @@ duckdb_extension_load(json)
 duckdb_extension_load(icu)
 duckdb_extension_load(httpfs
     GIT_URL https://github.com/duckdb/duckdb-httpfs
-    GIT_TAG 8356a9017444f54018159718c8017ff7db4ea756
+    GIT_TAG 39ebaf77e93a55b2bb839b621794eba49b2e359b
 )
