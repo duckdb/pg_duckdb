66c05405b9f08a9f2e1283afc1598e586bdd4cf3
Jelte Fennema-Nio <jelte@motherduck.com>
2025-12-12 10:51:15 +0100

    Update to DuckDB v1.4.3 (#985)

--- third_party/pg_duckdb_extensions.cmake
+++ third_party/pg_duckdb_extensions.cmake
@@ -2,5 +2,5 @@ duckdb_extension_load(json)
 duckdb_extension_load(icu)
 duckdb_extension_load(httpfs
     GIT_URL https://github.com/duckdb/duckdb-httpfs
-    GIT_TAG 39ebaf77e93a55b2bb839b621794eba49b2e359b
+    GIT_TAG 9c7d34977b10346d0b4cbbde5df807d1dab0b2bf
 )
