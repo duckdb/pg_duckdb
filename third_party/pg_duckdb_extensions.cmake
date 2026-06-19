934ccaef9d20190beddd14705edb0f73f79d1f9f
Jelte Fennema-Nio <jelte@motherduck.com>
2026-06-19 10:36:11 +0200

    Use 1.5.4 version of httpfs

--- third_party/pg_duckdb_extensions.cmake
+++ third_party/pg_duckdb_extensions.cmake
@@ -2,5 +2,5 @@ duckdb_extension_load(json)
 duckdb_extension_load(icu)
 duckdb_extension_load(httpfs
     GIT_URL https://github.com/duckdb/duckdb-httpfs
-    GIT_TAG 9de3296f40ed03e8e063394887f0d6a46144e847
+    GIT_TAG c3f215ab360f04dc3d3d5305fa81849c0121f111
 )
